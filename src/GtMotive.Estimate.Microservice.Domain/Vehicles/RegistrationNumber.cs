f4affe7ddcf23db3f18ac8d9e9046d9dd9d78e8f
OPENVIA\agomez <angel.gomez@digittude.com>
2026-08-16 11:14:15 +0200

    Added intelectual property advice to all files

--- src/GtMotive.Estimate.Microservice.Domain/Vehicles/RegistrationNumber.cs
+++ src/GtMotive.Estimate.Microservice.Domain/Vehicles/RegistrationNumber.cs
@@ -1,3 +1,16 @@
+/*
+ * Aviso de propiedad intelectual
+ *
+ * Este repositorio se ha creado exclusivamente como prueba tÃ©cnica para Capitole.
+ * Salvo los componentes de terceros y los derechos que pudieran haberse cedido
+ * expresamente por contrato, el cÃ³digo y la documentaciÃ³n originales contenidos en
+ * Ã©l son propiedad de su autor. No se autoriza su copia, reproducciÃ³n, modificaciÃ³n,
+ * distribuciÃ³n, publicaciÃ³n ni explotaciÃ³n, total o parcial, sin consentimiento
+ * previo y por escrito del titular de los derechos. El titular se reserva el
+ * ejercicio de las acciones legales que correspondan frente a cualquier uso no
+ * autorizado.
+ */
+
 using System;
 
 namespace GtMotive.Estimate.Microservice.Domain.Vehicles;
